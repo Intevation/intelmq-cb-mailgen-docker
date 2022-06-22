2964fbab58fa44951929457e07ec63f63224f440
Sebastian Wagner <swagner@intevation.de>
2022-06-22 12:32:39 +0200

    intelmq configuration: use 3.0 config style
    and remove the workarounds in the Dockerfile

--- intelmq/setup-certbund.sh
+++ intelmq/setup-certbund.sh
@@ -15,17 +15,5 @@ if [ "$USE_CERTBUND" = true ] ; then
   crontab /etc/cron.d/mailgen
 fi
 
-# remove pre-installed runtime.yaml and migrate our old 2.x-style configuration to the new format
-intelmq_major_version=$(sudo -u intelmq intelmqctl --version | cut -c 1)
-if [[ "$intelmq_major_version" -ge 3 ]]; then
-    # upgrade configuration to 3.x style
-    rm /opt/intelmq/etc/BOTS
-    rm /opt/intelmq/etc/runtime.yaml
-    sudo -u intelmq intelmqctl upgrade-config -f -u v300_defaults_file_removal
-    sudo -u intelmq intelmqctl upgrade-config -f -u v300_pipeline_file_removal
-    sed -i 's/intelmq_certbund_contact\.expert/intelmq.bots.experts.certbund_contact.expert/' /opt/intelmq/etc/runtime.yaml
-    sed -i 's/intelmq_certbund_contact\.ruleexpert/intelmq.bots.experts.certbund_rules.expert/' /opt/intelmq/etc/runtime.yaml
-fi
-
-# make any other necessary upgrades (harmonization)
+# make configuration upgrades if necessary
 sudo -u intelmq intelmqctl upgrade-config
