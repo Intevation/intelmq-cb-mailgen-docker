ab7bede4a3a2a79119dfefff7980c13249339d9c
Sebastian Wagner <swagner@intevation.de>
2022-06-28 17:44:36 +0200

    intelmq: fix connection to redis
    set the redis hostname in the runtime in certbund and non-certbund
    scenario in the configuration
    setting only the environment variable is not enough, as this is not
    effective in the API/Manager

--- intelmq/setup-certbund.sh
+++ intelmq/setup-certbund.sh
@@ -3,17 +3,35 @@
 set -eu -o pipefail
 
 if [ "$USE_CERTBUND" = true ] ; then
+  # install IntelMQ configuration files
   cp /opt/intelmq-config/* "$1"
-  crontab /etc/cron.d/mailgen
+  if [ "$1" == "/etc/intelmq" ]; then
+      sed -i 's@/opt/intelmq/var/lib/bots/file-output/@/var/lib/intelmq/bots/file-output/@' $1/runtime.yaml
+  fi
+else
+    # use default configuration, but set
+    # pipeline, cache and statistics hostname to "redis"
+    python3 << EOPY
+from ruamel.yaml import YAML
+yaml = YAML(typ="unsafe", pure=True)
+with open('$1/runtime.yaml', 'r+') as fpruntime:
+    runtime = yaml.load(fpruntime)
+    if 'global' not in runtime:
+        runtime['global'] = {}
+    runtime['global']['source_pipeline_host'] = runtime['global']['destination_pipeline_host'] = runtime['global']['redis_cache_host'] = runtime['global']['statistics_host'] = "redis"
+    fpruntime.seek(0)
+    for botid in runtime.keys():
+        if 'parameters' in runtime[botid].keys() and 'redis_cache_host' in runtime[botid]['parameters']:
+            del runtime[botid]['parameters']['redis_cache_host']
+    yaml.dump(runtime, fpruntime)
+EOPY
 fi
 
-if [ "$USE_CERTBUND" = true ] ; then
-  # install IntelMQ configuration files
-  cp /opt/intelmq-config/* $1
-  chown intelmq:intelmq /opt/intelmq/etc/*
-  chmod 664 /opt/intelmq/etc/*
-  crontab /etc/cron.d/mailgen
-fi
+chown -R intelmq:intelmq /etc/intelmq/*
+chmod -R 664 /etc/intelmq/*
+
+intelmqctl upgrade-config
+intelmqctl -q check --no-connections
 
 # make configuration upgrades if necessary
 sudo -u intelmq intelmqctl upgrade-config
--- intelmq/startpkg.sh
+++ intelmq/startpkg.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
 
-cron
+set -eu -o pipefail
+
+chown -R intelmq:intelmq /var/log/intelmq/ /var/lib/intelmq
+chown intelmq:intelmq /etc/intelmq/*
+
+intelmqctl upgrade-config
+intelmqctl check
+
 apachectl -D FOREGROUND
