545c5a452828d0b83de48b52986fb3bb5d99743f
Sebastian Wagner <swagner@intevation.de>
2023-02-15 14:42:30 +0100

    webinput test: make API calls working for all variants

--- webinput-csv/test.sh
+++ webinput-csv/test.sh
@@ -11,15 +11,19 @@ trap end_test ERR
 
 password=$(uuidgen)
 
-# check if the current setup has a separate backend container (dev). Disable the shell error trapping for the calls to grep
+# check if the current setup has a separate backend container (dev).
+# The packaged webinput-csv serves the software under /intelmq-webinput, whereas the dev version serves it under /
+# Disable the shell error trapping for the calls to grep
 set +e
 trap - ERR
 docker ps | grep intelmq-webinput-csv-backend
 if [ $? -eq 1 ]; then
     # has no backend
     webinput_csv_backend_container=intelmq-webinput-csv
+    API="http://localhost:1383/intelmq-webinput"
 else
     webinput_csv_backend_container=intelmq-webinput-csv-backend
+    API="http://localhost:1383"
 fi
 trap end_test ERR
 set -e
@@ -28,19 +32,19 @@ set -e
 docker exec -ti $webinput_csv_backend_container webinput-adduser --user second --password "$password"
 
 # Check Webinput CSV Frontend
-wget --no-verbose -O - http://localhost:1383/ > /dev/null
+wget --no-verbose -O - $API/ > /dev/null
 
 # Test Backend reachable via the /api path on the frontend port
 
 # Test Login
-token=$(wget --no-verbose -O - --post-data '{"username": "admin", "password": "secret"}' --header "Content-Type: application/json" http://localhost:1383/api/login/ 2>/dev/null | jq .login_token | tr -d '"')
+token=$(wget --no-verbose -O - --post-data '{"username": "admin", "password": "secret"}' --header "Content-Type: application/json" $API/api/login/ 2>/dev/null | jq .login_token | tr -d '"')
 
 # Query some data
-wget --no-verbose -O - --header "Authorization: $token" http://localhost:1383/api/classification/types | grep -o ids-alert
+wget --no-verbose -O - --header "Authorization: $token" $API/api/classification/types | grep -o ids-alert
 
-wget --no-verbose -O - --header "Authorization: $token" http://localhost:1383/api/harmonization/event/fields | grep -o source.ip
+wget --no-verbose -O - --header "Authorization: $token" $API/api/harmonization/event/fields | grep -o source.ip
 
-wget --no-verbose -O - --header "Authorization: $token" http://localhost:1383/api/custom/fields | grep -o '"feed.code": "oneshot"'
+wget --no-verbose -O - --header "Authorization: $token" $API/api/custom/fields | grep -o '"feed.code": "oneshot"'
 
 # Test injecting data
 docker exec -ti intelmq intelmqctl stop deduplicator-expert
@@ -48,7 +52,7 @@ docker exec -ti intelmq intelmqctl clear deduplicator-expert-queue
 
 # Use a non ISO-formatted date to test datetime parsing
 yesterday=$(date --rfc-email --date='yesterday')
-wget --no-verbose -O - --header "Authorization: $token" --header "Content-Type: application/json;charset=utf-8" --post-data "{\"timezone\":\"+00:00\",\"data\":[{\"time.source\":\" $yesterday \",\"source.ip\":\"192.168.56.7\",\"source.asn\":\"65537\"}],\"custom\":{\"custom_classification.type\":\"blacklist\",\"custom_classification.identifier\":\"test\",\"custom_feed.code\":\"oneshot\",\"custom_feed.name\":\"oneshot-csv\"},\"dryrun\":true}" http://localhost:1383/api/upload | grep 'lines_invalid": 0'
+wget --no-verbose -O - --header "Authorization: $token" --header "Content-Type: application/json;charset=utf-8" --post-data "{\"timezone\":\"+00:00\",\"data\":[{\"time.source\":\" $yesterday \",\"source.ip\":\"192.168.56.7\",\"source.asn\":\"65537\"}],\"custom\":{\"custom_classification.type\":\"blacklist\",\"custom_classification.identifier\":\"test\",\"custom_feed.code\":\"oneshot\",\"custom_feed.name\":\"oneshot-csv\"},\"dryrun\":true}" $API/api/upload | grep 'lines_invalid": 0'
 
 # test if the data was correctly passed on to IntelMQ
 result=$(docker exec -ti intelmq intelmqctl run deduplicator-expert message get | grep -Ev 'deduplicator-expert|Waiting for a message|time.observation')
