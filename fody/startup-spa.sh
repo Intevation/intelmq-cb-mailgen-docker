c93a072da28ec83a053792b8df6e08aee9a274ac
Sebastian Wagner <swagner@intevation.de>
2023-02-15 17:31:16 +0100

    fody startup script: fix proxy to backend
    in source and dev scenario, proxy the api calls to the correct container
    pkg and full-pkg work fine out of the box

--- fody/startup-spa.sh
+++ fody/startup-spa.sh
@@ -4,5 +4,11 @@ set -xeu -o pipefail
 
 echo "$PWD"
 
+# in scenarios source and dev the backend can be reached in the docker network via this address-port-combination
+# in source scenario this could be done at build time, but not in dev scenario
+# as the volume overlay is mapped at runtime
+# this file is not executed in the pkg and full-pkg scenarios
+sed -i "s#target: .*#target: 'http://intelmq-fody-backend:8002',#" config/index.js
+
 yarn 2>&1
 yarn run dev 2>&1
