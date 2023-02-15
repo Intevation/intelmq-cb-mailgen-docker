3437684db48636aa203cc4daabe0c3dd37715b0e
Sebastian Wagner <swagner@intevation.de>
2023-02-15 18:21:03 +0100

    webinput-csv startup script: fix proxy to backend
    in source and dev scenario, proxy the api calls to the correct container
    pkg and full-pkg work fine out of the box

--- webinput-csv/startup-spa.sh
+++ webinput-csv/startup-spa.sh
@@ -4,5 +4,11 @@ set -xeu -o pipefail
 
 echo "$PWD"
 
+# in scenarios source and dev the backend can be reached in the docker network via this address-port-combination
+# in source scenario this could be done at build time, but not in dev scenario
+# as the volume overlay is mapped at runtime
+# this file is not executed in the pkg and full-pkg scenarios
+sed -i "s#target: .*#target: 'http://intelmq-webinput-csv-backend:8002',#" /opt/src/intelmq-webinput-csv/client/vue.config.js
+
 yarn
 yarn serve
