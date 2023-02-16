ad60fec80021fa47f7f55c2229fd2a4f821758cf
Sebastian Wagner <swagner@intevation.de>
2023-02-16 11:25:40 +0100

    intelmq startup script: create api user
    in dev scenario the user was not created

--- intelmq/startup.sh
+++ intelmq/startup.sh
@@ -3,6 +3,10 @@
 set -xeu -o pipefail
 
 cd /opt/intelmq-api
+
+# required for dev scenario
+./scripts/intelmq-api-adduser --user admin --password secret
+
 hug -p 81 -m intelmq_api.serve &
 apachectl -D FOREGROUND
 
