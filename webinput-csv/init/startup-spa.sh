b06188581a0a30c72e391153cd79de546c46ded3
Sebastian Wagner <swagner@intevation.de>
2023-03-13 18:14:28 +0100

    webinput csv: use /opt, not /opt/src as base directory
    as all the other programs too

--- webinput-csv/init/startup-spa.sh
+++ webinput-csv/init/startup-spa.sh
@@ -8,7 +8,7 @@ echo "$PWD"
 # in source scenario this could be done at build time, but not in dev scenario
 # as the volume overlay is mapped at runtime
 # this file is not executed in the pkg and full-pkg scenarios
-sed -i "s#target: .*#target: 'http://intelmq-webinput-csv-backend:8002',#" /opt/src/intelmq-webinput-csv/client/vue.config.js
+sed -i "s#target: .*#target: 'http://intelmq-webinput-csv-backend:8002',#" /opt/intelmq-webinput-csv/client/vue.config.js
 
 yarn
 yarn serve
