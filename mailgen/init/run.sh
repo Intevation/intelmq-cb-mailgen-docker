70a0fe40ddd17a1ad75a86ac6c27a7c8e6ef31dc
Sebastian Wagner <swagner@intevation.de>
2023-03-13 19:51:49 +0100

    mailgen docker: install pkg-resources
    until https://github.com/Intevation/intelmq-mailgen/commit/65aa510efb0bfa91afd21109404ec02fcd73b750 comes into effect

--- mailgen/init/run.sh
+++ mailgen/init/run.sh
@@ -4,10 +4,15 @@ set -eu -o pipefail
 
 wait-for-it.sh database:5432 -t 0 -s
 
-cd /opt/intelmq-mailgen/
-# for dev scenario:
-# ensure /opt/intelmq-mailgen/intelmqmail.egg-info/ exists, so that the package can be loaded
-pip3 install -e .
+# only in non-pkg scenarios
+if [ -d /opt/intelmq-mailgen/ ]; then
+    cd /opt/intelmq-mailgen/
+    # for dev scenario:
+    # ensure /opt/intelmq-mailgen/intelmqmail.egg-info/ exists, so that the package can be loaded
+    pip3 install -e .
+else
+    apt install python3-pkg-resources
+fi
 
 while /bin/true ; do
     if [[ -f /tmp/intelmqcbmail_disabled ]]; then
