e5b9807d004ba461af1046551e2e45430fb7319d
Sebastian Wagner <swagner@intevation.de>
2023-03-16 10:50:18 +0100

    intelmq startup dev: fix installation of certbund

--- intelmq/startup.sh
+++ intelmq/startup.sh
@@ -2,10 +2,10 @@
 
 set -xeu -o pipefail
 
-# for dev scenario (../intelmq/ is mounted to /opt/intelmq) this needs to be done at runtime
+# for dev scenario (../intelmq/ is mounted to /opt/intelmq_src) this needs to be done at runtime
 # with editable installations, installing a second intelmq package (for the intelmq/bots/experts/certbund* structure) does not work. So we can work around by symlinking the files
-# if destination already exists, -u prevents an error
-bash -c "cp -urs /opt/intelmq-certbund-contact/intelmq/bots/experts/certbund_rules /opt/intelmq_src/intelmq/bots/experts/ && cp -urs /opt/intelmq-certbund-contact/intelmq/bots/experts/certbund_contact /opt/intelmq_src/intelmq/bots/experts/"
+# if destination already exists, -f prevents an error
+bash -c "cp -frs /opt/intelmq-certbund-contact/intelmq/bots/experts/certbund_rules /opt/intelmq_src/intelmq/bots/experts/ && cp -frs /opt/intelmq-certbund-contact/intelmq/bots/experts/certbund_contact /opt/intelmq_src/intelmq/bots/experts/"
 # create necessary directories
 bash -c "mkdir -p /opt/intelmq/var/{log/,lib/} && chown -R intelmq:intelmq /opt/intelmq/var /opt/intelmq/etc"
 
