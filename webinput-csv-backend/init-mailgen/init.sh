8f8274b8f24fad213a5307570865da6ffea565d7
Sebastian Wagner <swagner@intevation.de>
2023-05-11 12:14:17 +0200

    webinput-mailgen: write app output to log files

--- webinput-csv-backend/init-mailgen/init.sh
+++ webinput-csv-backend/init-mailgen/init.sh
@@ -10,9 +10,9 @@ set -eu -o pipefail
 /opt/mailgen-init/run.sh &
 
 # webinput backend
-# Start the backend using hug. 
-hug -f /opt/intelmq-webinput-csv/intelmq_webinput_csv/serve.py -p 8002 &
+# Start the backend using hug.
+hug -f /opt/intelmq-webinput-csv/intelmq_webinput_csv/serve.py -p 8002 2>&1 | tee -a /var/log/webinput_csv_backend.log &
 
 # webinput client
 cd /opt/intelmq-webinput-csv/client/
-/opt/webinput-csv-init/startup-dev.sh
+/opt/webinput-csv-init/startup-dev.sh | tee -a /var/log/webinput_csv.log
