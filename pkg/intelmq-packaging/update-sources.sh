f3c005514eddd5ee15eb65fa2b3a0de20f6ea7a1
Sebastian Wagner <swagner@intevation.de>
2023-08-22 14:01:30 +0200

    pkg build: add intelmq and current yarn repo
    some packages require intelmq for the build (test requirement),
    so add the intelmq repository
    
    the yarn repository had a outdated key,
    add the repo and the key in the same way as the intelmq repository

--- pkg/intelmq-packaging/update-sources.sh
+++ pkg/intelmq-packaging/update-sources.sh
@@ -3,6 +3,7 @@
 echo 'deb https://deb.nodesource.com/node_14.x focal main' \
      >/etc/apt/sources.list.d/nodesource.list
 apt-key add /tmp/nodesource.gpg.key
-echo 'deb https://dl.yarnpkg.com/debian/ stable main' \
-     >/etc/apt/sources.list.d/yarn.list
-apt-key add /tmp/yarnpkg.gpg.key
+echo "deb http://download.opensuse.org/repositories/home:/sebix:/intelmq/xUbuntu_20.04/ /" > /etc/apt/sources.list.d/intelmq.list
+curl -sSL https://download.opensuse.org/repositories/home:sebix:intelmq/xUbuntu_20.04/Release.key | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" > /etc/apt/sources.list.d/yarn.list
+curl -sSL https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
