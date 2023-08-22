e1d1ee74a0dab69a276ae283572f1d6a0d814964
Sebastian Wagner <swagner@intevation.de>
2023-08-22 14:09:37 +0200

    setup-apt: extract OS and version from /etc
    use /etc/os-release to get the OS name and version
    use them to select the proper repositories

--- common/setup-apt.sh
+++ common/setup-apt.sh
@@ -2,6 +2,8 @@
 
 which wget > /dev/null 2>&1
 dependencies_installed=$?
+
+# defines $ID, $VERSION_ID
 . /etc/os-release
 
 set -xeu -o pipefail
@@ -11,26 +13,32 @@ if [ "$dependencies_installed" -ne 0 ]; then
     apt-get update && DEBIAN_FRONTEND="noninteractive" apt-get install -y wget apt-transport-https gnupg
 fi
 
+if [ "$ID" == "debian" ]; then
+    os_repo_name="Debian"
+elif [ "$ID" == "ubuntu" ]; then
+    os_repo_name="xUbuntu"
+fi
+
 if [ "$1" == "intelmq" ]; then
     # Add sebix repo to sources
-    echo "deb http://download.opensuse.org/repositories/home:/sebix:/intelmq/xUbuntu_${VERSION_ID}/ /" >> /etc/apt/sources.list.d/intelmq.list
-    wget -O - https://download.opensuse.org/repositories/home:sebix:intelmq/xUbuntu_${VERSION_ID}/Release.key | apt-key add -
+    echo "deb http://download.opensuse.org/repositories/home:/sebix:/intelmq/${os_repo_name}_${VERSION_ID}/ /" > /etc/apt/sources.list.d/intelmq.list
+    wget -O - https://download.opensuse.org/repositories/home:sebix:intelmq/${os_repo_name}_${VERSION_ID}/Release.key | apt-key add -
     if [ "${INTELMQ_UNSTABLE_REPOSITORY:-false}" == "true" ]; then
-        echo "deb http://download.opensuse.org/repositories/home:/sebix:/intelmq:/unstable/xUbuntu_${VERSION_ID}/ /" >> /etc/apt/sources.list.d/intelmq.list
-        wget -O - https://download.opensuse.org/repositories/home:sebix:intelmq:unstable/xUbuntu_${VERSION_ID}/Release.key | apt-key add -
+        echo "deb http://download.opensuse.org/repositories/home:/sebix:/intelmq:/unstable/${os_repo_name}_${VERSION_ID}/ /" >> /etc/apt/sources.list.d/intelmq.list
+        wget -O - https://download.opensuse.org/repositories/home:sebix:intelmq:unstable/${os_repo_name}_${VERSION_ID}/Release.key | apt-key add -
     fi
 elif [ "$1" == "intevation" ]; then
     # Add Intevation apt repo
-    echo "deb https://apt.intevation.de focal intelmq-testing" > /etc/apt/sources.list.d/intevation.list
+    echo "deb https://apt.intevation.de ${VERSION_CODENAME} intelmq-testing" > /etc/apt/sources.list.d/intevation.list
     wget -O - https://ssl.intevation.de/Intevation-Distribution-Key-2021.asc | apt-key add -
 elif [ "$1" == "node" ]; then
-    echo "deb https://deb.nodesource.com/node_14.x focal main" >/etc/apt/sources.list.d/nodesource.list
+    echo "deb https://deb.nodesource.com/node_14.x ${VERSION_CODENAME} main" > /etc/apt/sources.list.d/nodesource.list
     wget -O -  https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
 elif [ "$1" == "local" ]; then
     DEBIAN_FRONTEND="noninteractive" apt-get install -y dpkg-dev
     # Add local file system repository and favor it over other sources
     echo deb [trusted=yes] file:/opt/packages ./ > /etc/apt/sources.list.d/local.list
-    echo "Package: *\nPin: origin \"\"\nPin-Priority: 999" > /etc/apt/preferences.d/local-repository
+    echo -e "Package: *\nPin: origin \"\"\nPin-Priority: 999" > /etc/apt/preferences.d/local-repository
     # Create a Packages file
     cd /opt/packages
     dpkg-scanpackages -m . > Packages
