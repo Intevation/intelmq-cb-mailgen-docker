dcc6da247923c2c213696666b8153429de51998a
Sebastian Wagner <swagner@intevation.de>
2023-09-08 08:59:01 +0200

    mailgen oneshot format: use default table spec

--- mailgen/formats/oneshot/10oneshot.py
+++ mailgen/formats/oneshot/10oneshot.py
@@ -5,23 +5,6 @@ with the event information in CSV format where the columns are limited
 to event attributes that should be present in almost all events.
 """
 
-from intelmqmail.tableformat import build_table_format
-#from intelmqmail.templates import Template, read_template, full_template_filename
-#from intelmqmail.notification import Postponed
-
-
-table_format = build_table_format(
-    "Fallback",
-    (("source.asn", "asn"),
-     ("source.ip", "ip"),
-     ("time.source", "timestamp"),
-     ("source.port", "src_port"),
-     ("destination.ip", "dst_ip"),
-     ("destination.port", "dst_port"),
-     ("destination.fqdn", "dst_host"),
-     ("protocol.transport", "proto"),
-     ))
-
 
 def create_notifications(context):
     # always create notifications, never postpone
@@ -30,7 +13,4 @@ def create_notifications(context):
     # above template, add them to the substitutions dictionary. By
     # passing it to the mail_format_as_csv method below they will be
     # substituted into the template when the mail is created.
-    substitutions = dict()
-
-    return context.mail_format_as_csv(table_format,
-                                      substitutions=substitutions)
+    return context.mail_format_as_csv(substitutions={})
